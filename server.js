6d54127f7f4a1c38b9df13c6c29f7b4bb6ea2b89
Adam Selph <adam@selph.net>
2022-10-18 14:47:32 -0400

    fix file structure

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const parksController = require('./controllers/parksController');
+const PATH = '3333';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended :true }));
+app.use(express.static('dist'));
+
+// app.get('/', (req, res) => {
+//     return res.status(200).sendFile(path.resolve(__dirname, '../views/wherevertheyare'))
+// });
+
+app.get('/', (req, res) => {
+  return res.status(200).json(res.locals.parks);
+});
+
+//404 catch-all
+app.use((req, res) => {
+  return res.status(404).send('404 error found');
+});
+
+//global (not for json DOM)  
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'express error handler  caght unknown middleware error, check middleware logic',
+    status: 500,
+    message: { err: 'this is the default error handler'},
+  };
+  const updatedObj = Object.assign({}, defaultErr, err);
+  return res.status(updatedObj.status).json(updatedObj.message);
+});
+  
+
+app.listen(PORT => () => console.log(`server connected ${PORT}`))
