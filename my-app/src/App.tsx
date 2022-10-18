c75b3e244615c35a8b35e3531f7e7396bae3998b
Anshuman Sinha <anshuman@Anshumans-MacBook-Air.local>
2022-10-18 11:23:39 -0500

    Feat: Initialization of fullstack application

---
+++ my-app/src/App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import LoginBox from './Components/LoginBox'
+import BucketList from './Components/BucketList';
+import ParkList from './Components/ParkList'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#26812a',
+    },
+    secondary: {
+      main: '#cb1459',
+    },
+  },
+});
+
+function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <header className="App-header">
+          <p>Hello World</p>
+          <LoginBox/>
+        </header>
+        <BucketList/>
+        <ParkList/>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/ParkList.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ParkList() {
+  return (
+    <div>
+      ParkList
+    </div>
+  )
+}
+
+export default ParkList;
