d45fabce752d9d8269cbff575501c1af312c7640
Anshuman Sinha <anshuman@Anshumans-MacBook-Air.local>
2022-10-19 14:57:22 -0500

    Styled Front-end and updated some interactivity for a more seamless user experience.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import './StyleSheets/App.css';
 import "./Components/styles.css";
-import LoginButton from './Components/LoginButton';
+// import LoginButton from './Components/LoginButton';
 import BucketList from './Components/BucketList';
 import ParkList from './Components/ParkList'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import LoginForm from './Components/LoginForm';
+import WispnLogo from './images/Wispn.png';
+// import LoginForm from './Components/LoginForm';
 
 const theme = createTheme({
   palette: {
@@ -19,22 +20,23 @@ const theme = createTheme({
 });
 
 function App() {
-  const [isShowLogin, setIsShowLogin] = useState(true);
+  // const [isShowLogin, setIsShowLogin] = useState(true);
 
-  const handleLoginClick = () => {
-    setIsShowLogin((isShowLogin) => !isShowLogin);
-  };
+  // const handleLoginClick = () => {
+  //   setIsShowLogin((isShowLogin) => !isShowLogin);
+  // };
 
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <header className="App-header">
-          <p>Hello World</p>
-          <LoginButton handleLoginClick={handleLoginClick} />
-          <LoginForm isShowLogin={isShowLogin} />
+        <header className="App-header" >
+          <img src={WispnLogo} alt='Wispn Logo' className='wispn-logo'/>
         </header>
-        <BucketList/>
-        <ParkList/>
+        <div className="Body">
+          <BucketList/>
+          <ParkList/>
+        </div>
+
       </ThemeProvider>
     </div>
   );
