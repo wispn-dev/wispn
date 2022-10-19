d43edeee6a353fb141622e2a242e8e2a386387a1
Anshuman Sinha <anshuman@Anshumans-MacBook-Air.local>
2022-10-19 09:19:50 -0500

    Created ActivityBox/BucketList components and tested functionality and http requests

--- src/Components/ActivityBox.tsx
+++ src/Components/ActivityBox.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Checkbox } from "@mui/material";
+import { useAppSelector, useAppDispatch } from '../hooks';
+import { toggleActivity } from '../reducers';
 
-function ActivityBox() {
+function ActivityBox(props: {activity: string}) {
     // useState hooks to later identify which checkboz is being selected?
-    const [checked, setChecked] = React.useState(true);
+    // const [checked, setChecked] = React.useState(true);
+    const allActivities = useAppSelector((state)=>state.activities);
+    const dispatch = useAppDispatch();
+    const checked = allActivities[props.activity];
+
+    
+
     const handleChange = (event:any) => {
-        setChecked(event.target.checked);
+      dispatch(toggleActivity({activity: props.activity, status: !checked}));
   };
 
   return (
-    <Checkbox defaultChecked/>
+    <Checkbox checked={checked} onChange={handleChange} />
   )
 }
 
 export default ActivityBox;
+
--- src/Components/BucketList.tsx
+++ src/Components/BucketList.tsx
@@ -2,31 +2,34 @@ import React, {useEffect} from 'react';
 import ActivityBox from './ActivityBox';
 import { FormControlLabel, FormGroup } from "@mui/material";
 import { useAppDispatch, useAppSelector } from '../hooks';
+import { setActivities } from '../reducers';
 
 
 function BucketList() {
   // access state here -> useAppSelector to access state?
-  const allActivities = {'running': false, 'swimming': false};
-  // const allActivities = useAppSelector((state)=>state.activities)
+  // const allActivities = {'running': false, 'swimming': false};
+  const allActivities = useAppSelector((state)=>state.activities);
   const dispatch = useAppDispatch();
   
   const checkboxes: JSX.Element[] = [];
   // for each activity in the state
     // push to checkboxes {<FormControlLabel control={<ActivityBox />} label="Check me"/>}
   Object.keys(allActivities).forEach(activity => {
-    checkboxes.push(<FormControlLabel control={<ActivityBox />} label={activity}/>)
+    checkboxes.push(<FormControlLabel control={<ActivityBox activity={activity}/>} label={activity}/>)
   });
 
   useEffect(() => {
     // make api call to get the activities <-- written by backend team
     const fetchActivities = async () => {
       const response = await fetch('/activities');
-      
+      const activities = await response.json();
+      dispatch(setActivities(activities));
+
     }
     // set the activites state to the fetched activities
 
     fetchActivities();
-  }, [])
+  }, [dispatch])
 
   
 
@@ -42,3 +45,48 @@ function BucketList() {
 }
 
 export default BucketList;
+
+/*
+[
+  "Arts and Culture",
+  "Astronomy",
+  "Auto and ATV",
+  "Biking",
+  "Boating",
+  "Camping",
+  "Canyoneering",
+  "Caving",
+  "Climbing",
+  "Compass and GPS",
+  "Dog Sledding",
+  "Fishing",
+  "Flying",
+  "Food",
+  "Golfing",
+  "Guided Tours",
+  "Hands-On",
+  "Hiking",
+  "Horse Trekking",
+  "Hunting and Gathering",
+  "Ice Skating",
+  "Junior Ranger Program",
+  "Living History",
+  "Museum Exhibits",
+  "Paddling",
+  "Park Film",
+  "Playground",
+  "SCUBA Diving",
+  "Shopping",
+  "Skiing",
+  "Snorkeling",
+  "Snow Play",
+  "Snowmobiling",
+  "Snowshoeing",
+  "Surfing",
+  "Swimming",
+  "Team Sports",
+  "Tubing",
+  "Water Skiing",
+  "Wildlife Watching"
+  ]
+  */
